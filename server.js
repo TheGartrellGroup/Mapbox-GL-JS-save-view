ba0723c50182cae047b46e81603173fc7ebfe520
unknown <ben.sainsbury@gartrellgroup.com>
2017-02-14 16:19:28 -0800

    Added tests for view route

--- server.js
+++ server.js
@@ -4,14 +4,15 @@ var express = require('express'),
 jsonfile = require('jsonfile'),
 //sqlite3 = require('sqlite3'),
 uuid = require('node-uuid'),
-Validator = require('jsonschema').Validator;
+Validator = require('jsonschema').Validator,
+bodyParser = require('body-parser')
 
 var v = new Validator();
 
-var instance = 4;
-
-var mapViewSchema = {
-	"map": {
+var mapSchema = {
+	"id":'/mapSchema',
+	"type":"object",
+	"properties":{
 		"center": {
 			"type": "array",
 			"items": {
@@ -31,73 +32,108 @@ var mapViewSchema = {
 		"pitch": {
 			"type": "number"
 		}
-	},
-	"layers": {
-		"type": "array",
-		"items": {
-			"id": {
-				"type": "string",
-				"required": true
-			},
-			"visibility": {
-				"type": "string",
-				"pattern": /(visibility|none)/,
-				"required": true
-			},
-			"paint": {
-				"type": "object"
-			},
-			"filter": {
-				"type": "object"
-			}
+	}
+}
+
+var layerSchema  = {
+	"type":"object",
+	"properties":{
+		"id": {
+			"type": "string",
+			"required": true
+		},
+		"visibility": {
+			"type": "string",
+			"pattern": /(visibility|none)/,
+			"required": true
+		},
+		"paint": {
+			"type": "object"
+		},
+		"filter": {
+			"type": "object"
 		}
 	}
 }
 
+
+var mapStateSchema = {
+	"type":"object",
+	"properties":{
+		"map" : {"$ref": "/mapSchema"},
+		"layers": {'type':"array", "items":{"$ref": "layerSchema"} }
+	},
+	"required":["map", "layers"],
+	"additionalProperties": false
+}
+
+v.addSchema(mapSchema, '/mapSchema');
+v.addSchema(layerSchema, '/layerSchema');
+
 /* global */
 var app = express()
 
-app.get('/:id*?', function(req, res){
-
-	//server side rendering here //
-
-	//client side view acquisition here
-	
-	res.sendFile('index.html', {root: __dirname});
-});
+app.use(bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+})); 
 
 /* write views via post */
 app.post('/view/', function (req, res) {
   
-	//validate object
-	if(!req.body.view){
-		res.status(400).end('{"status": "failure", "message":"Must provide a view object."}');
-	} 
+	res.header('Content-Type', 'application/javascript');
 
-	if(!isJSON(req.body.view)){
-		res.status(400).end('{"status": "failure", "message":"Not a valid JSON object."}');
+	//validate object
+	if(!req.body){
+		return res.status(400).end('{"status": "failure", "message":"Must provide a JSON view object."}');
 	}
 
-	var view_object = JSON.parse(req.body.view)
+	if(typeof(req.body) !== 'object'){
+		return res.status(400).end('{"status": "failure", "message":"Must provide a JSON object."}');
+		
+	}
 
-	var valid = v.validate(req.body.view, mapViewSchema)
+	var valid = v.validate(req.body, mapStateSchema)
 
 	if(valid.errors.length){
-		var error_message = valid.errors.reduce(function(acc, err){acc += ' '+err.stack})
-		res.status(400).end('{"status": "failure", "message":"'+error_message+'"}');
+		var error_message = valid.errors.reduce(function(acc, err){return acc += ', '+err.stack.replace('"','')})
+		return res.status(400).end('{"status": "failure", "message":"'+error_message+'"}');
 	}
 
-	var id = uuid.v1().substr(0, 8);
+	var id = uuid.v1().substr(0, 8); 
 
-	jsonfile.writeFile('/data/'+id+'.json', function(err){
-		res.status(200).end('{"status": "success", "message":"Saved view"}');
+	jsonfile.writeFile('./data/'+id+'.json',req.body, function(err){
+
+		return res.status(200).end('{"status": "success", "message":"Saved view", "id":"'+id+'"}');
 	})
 	
 });
 
 /* read views via get */
-app.get('/view/:id', function (req, res) {
-	res.send(__dirname + 'views/'+req.params.id)
+app.get('/view/:id*?', function (req, res) {
+
+	res.header('Content-Type', 'application/javascript');
+
+	if(!req.params.id){
+		return res.status(400).end('{"status": "failure", "message":"Must provide an id for a view."}');
+	}
+
+	jsonfile.readFile('./data/'+req.params.id+'.json', function(err, obj){
+		if(err){
+			return res.status(400).end('{"status": "failure", "message":"View not found"}');
+		} else {
+			return res.send(obj);
+		}
+	})
+});
+
+app.get('/:id*?', function(req, res){
+
+	//could do server side rendering here //
+
+	//client side view acquisition here
+
+	res.sendFile('index.html', {root: __dirname});
 });
 
 app.use(function(req,res){
@@ -108,11 +144,4 @@ app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 })
 
-function isJSON(str){
-    try {
-        JSON.parse(str);
-    } catch (e) {
-        return false;
-    }
-    return true;
-}
+module.exports = app
--- test/app.request.js
+++ test/app.request.js
@@ -1,20 +1,150 @@
-// test/app.request.js
-it('responds with html', function (done) {
-  request(app)
-    .get('/')
-    .expect('Content-Type', /html/)
-    .expect(200, done);
-});
-it('responds with javascript', function (done) {
-  request(app)
-    .get('/scripts/index.js')
-    .expect('Content-Type', /javascript/)
-    .expect(200, done);
-});
-it('responds with json', function (done) {
-  request(app)
-    .get('/')
-    .set('X-Requested-With', 'XMLHttpRequest')
-    .expect('Content-Type', /json/)
-    .expect(200, done);
+var request = require('supertest'),
+  app = require('../server.js');
+
+describe('GET app', function () {
+  
+
+/*
+_  _ _ ____ _ _ _    ___  ____ ____ ___ 
+|  | | |___ | | |    |__] |  | [__   |  
+ \/  | |___ |_|_|    |    |__| ___]  |  
+                                        */
+
+  it('400 JSON error if I post to views without anything', function (done) {
+    request(app)
+      .post('/view/')
+      .expect('Content-Type', /javascript/)
+      .expect(400)
+      .end(function(err, res){
+        console.log(res.error.text)
+        done()
+      });
+  });
+
+   it('400 JSON error if I post to views with bad data', function (done) {
+    request(app)
+      .post('/view/')
+      .send('yiuppessdfsdf;lksjdflskdjflksdfj')
+      .expect('Content-Type', /javascript/)
+       .expect(400)
+      .end(function(err, res){
+        console.log(res.error.text)
+        done()
+      });
+  });
+
+
+  it('400 JSON error if I post a completely invalid object to view', function (done) {
+    request(app)
+      .post('/view/')
+      .send({"foo":"bar"})
+      .expect('Content-Type', /javascript/)
+      .expect(400)
+      .end(function(err, res){
+        console.log(res.error.text)
+        done()
+      });
+
+  });
+
+  var mapStateBad = {
+      "map":{
+          "center": [-123.543667, 43.76544],
+          "zoom": "yay",
+          "bearing" : -21.5,
+          "pitch": 15
+      },
+      "layers": [{
+          "id": "state-boundaries",
+          "visibility": "none",
+          "paint": {},
+          "filter": {}
+      }]
+  }
+
+  it('400 JSON error if I post a partially invalid object to view', function (done) {
+    request(app)
+      .post('/view/')
+      .send(mapStateBad)
+      .expect('Content-Type', /javascript/)
+      .expect(400)
+      .end(function(err, res){
+        console.log(res.error.text)
+        done()
+      });
+  });
+
+
+ var mapStateGood = {
+      "map":{
+          "center": [-123.543667, 43.76544],
+          "zoom": 12,
+          "bearing" : -21.5,
+          "pitch": 15
+      },
+      "layers": [{
+          "id": "state-boundaries",
+          "visibility": "none",
+          "paint": {},
+          "filter": {}
+      }]
+  }
+
+    it('should 200 when posting valid mapState object', function (done) {
+    request(app)
+      .post('/view/')
+      .send(mapStateGood)
+      .expect('Content-Type', /javascript/)
+      .expect(400)
+      .end(function(err, res){
+        console.log(res.text)
+        done()
+      });
+    });
+
+
+/*
+_  _ _ ____ _ _ _    ____ ____ ___ 
+|  | | |___ | | |    | __ |___  |  
+ \/  | |___ |_|_|    |__] |___  |  
+                                  */
+
+
+    it('should 400 when getting unspecified id', function (done) {
+    request(app)
+      .get('/view/')
+      .expect('Content-Type', /javascript/)
+      .expect(400)
+      .end(function(err, res){
+        console.log(res.text)
+        done()
+      });
+    });
+
+     it('should 400 when getting bad id', function (done) {
+    request(app)
+      .get('/view/1234567')
+      .expect('Content-Type', /javascript/)
+      .expect(400)
+      .end(function(err, res){
+        console.log(res.text)
+        done()
+      });
+    });
+
+  it('should 200 when getting valid id', function (done) {
+    request(app)
+      .get('/view/cf024e80')
+      .expect('Content-Type', /javascript/)
+      .expect(200)
+      .end(function(err, res){
+        console.log(res.text)
+        done()
+      });
+    });
+
+
+
+
+
 });
