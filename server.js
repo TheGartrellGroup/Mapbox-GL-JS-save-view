a13810a0a74aa4a4812395d4894d6b1c13472eea
unknown <ben.sainsbury@gartrellgroup.com>
2017-02-16 11:39:12 -0800

    Fixed shapes loading and optional save

--- server.js
+++ server.js
@@ -7,7 +7,21 @@ uuid = require('node-uuid'),
 Validator = require('jsonschema').Validator,
 bodyParser = require('body-parser')
 
-var SAVETO = 'JSONFILE' // or 'SQLITE'
+var SAVETO = 'JSONFILE';
+//SAVETO = 'SQLITE';
+
+var sqlite3, db;
+var readView, writeView;
+if(SAVETO === 'SQLITE'){
+
+	sqlite3 = require('sqlite3')
+	db = new sqlite3.Database('db/views.db');
+	readView = _readViewFromSQLiteDB;
+	writeView = _writeViewToSQLiteDB;
+} else {
+	readView = _readViewFromJSONFile;
+	writeView = _writeViewToJSONFile;
+}
 
 var v = new Validator();
 
@@ -101,15 +115,13 @@ app.post('/view/', function (req, res) {
 		return res.status(400).end('{"status": "failure", "message":"'+error_message+'"}');
 	}
 
-	if(SAVETO === 'JSONFILE'){
-		_writeViewToJSONFile(req.body, function( msg){
-			return res.status(200).end(msg)
-		});
-	} else {
-		_writeViewToSQLiteDB(req.body, function(msg){
-			return res.status(200).end(msg)
-		})
-	}
+	writeView(req.body, function(err, id){
+		if(err){
+			return res.status(400).end('{"status" : "failure", "msg": "' + err + '"}');
+		} else {
+			return res.status(200).end('{"status" :"success", "msg":"Saved view", "id":"'+id+'"}');
+		}
+	});
 
 });
 
@@ -122,13 +134,14 @@ app.get('/view/:id*?', function (req, res) {
 		return res.status(400).end('{"status": "failure", "message":"Must provide an id for a view."}');
 	}
 
-	jsonfile.readFile('./data/'+req.params.id+'.json', function(err, obj){
+	readView(req.params.id, function(err, obj){
 		if(err){
 			return res.status(400).end('{"status": "failure", "message":"View not found"}');
 		} else {
-			return res.send(obj);
+			return res.status(200).send(obj);
 		}
-	})
+	});
+	
 });
 
 app.get('/:id*?', function(req, res){
@@ -144,7 +157,7 @@ app.use(function(req,res){
 	res.status(500).end('{"status": "failure", "message":"Please either POST data to the /view/ function or GET /view/ with a state id"}');
 });
 
-app.listen(4001, function () {
+app.listen(4005, function () {
   console.log('Example app listening on port 3000!')
 })
 
@@ -152,31 +165,48 @@ function _writeViewToJSONFile(jsonobj, cb){
 
 	var id = uuid.v1().substr(0, 8); 
 
-	jsonfile.writeFile('./data/'+id+'.json',jsonobj, function(err){
+	jsonfile.writeFile('./data/'+id+'.json',jsonobj, function(err, msg){
 		if(err){
-			cb('{"status": "error", "message":"'+err+'"}')
+			cb(err)
 		} else {
-			cb('{"status": "success", "message":"Saved view", "id":"'+id+'"}');
+			cb(null, id);
+		}
+	})
+}
+
+function _readViewFromJSONFile(id, cb){
+	jsonfile.readFile('./data/'+id+'.json', function(err, obj){
+		if(err){
+			cb(err)
+		} else {
+			cb(null, obj)
 		}
 	})
 }
 
 function _writeViewToSQLiteDB(jsonObj, cb){
 
-	sqlite3 = require('sqlite3')
-
-	var db = 'db/views.db';
-
 	var id = uuid.v1().substr(0, 8); 
 
-	db.exec("insert into views (id, view) values ('"+id+"', '" +JSON.stringify(jsonObj)+ "')", function(err, row) {
+	db.exec("INSERT into views (id, view) values ('"+id+"', '" +JSON.stringify(jsonObj)+ "')", function(err, row) {
 
 		if(err){
-			cb('{"status" : "failure", "msg": "'+err+'"}');
+			cb(err);
 		} else {
-			cb('{"status" :"success", "msg":"Saved view '+id+'"}');
+			cb(null, id);
 		}
-	})
+	});
+}
+
+function _readViewFromSQLiteDB(id, cb){
+
+	db.get("SELECT view from views where id='"+id+"'", function(err, row){
+		if(err || !row){
+			cb(err)
+		} else {
+			cb(null, row.view)
+		}
+	});
 }
 
 module.exports = app
